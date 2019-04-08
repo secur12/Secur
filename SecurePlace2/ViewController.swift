01db0f1ce94954c4c0f78225fa1b881dc9350ca3
Oleksandr Bambuliak <yosestartup@gmail.com>
2019-04-08 10:50:53 +0200

    Initial Commit

---
+++ SecurePlace2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecurePlace2
+//
+//  Created by Oleksandr Bambulyak on 08.04.2019.
+//  Copyright © 2019 Oleksandr Bambulyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
